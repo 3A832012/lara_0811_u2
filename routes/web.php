f821e3eb18ddba9810307e99e0c5f0483282ebc6
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-10-18 21:49:01 +0800

    修改 Route 接受參數

--- routes/web.php
+++ routes/web.php
@@ -13,12 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('r2',function(){
-    return view('welcome');
-});
-
-Route::get('r1',function(){
-    return redirect('r2');
+Route::get('hello/{name}',	function($name)	{
+    return	'Hello,	'.$name;
 });
 
 
