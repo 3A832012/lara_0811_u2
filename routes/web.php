d13187174bee9198210ca13f48e0faa19aeefe0b
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-10-18 21:40:54 +0800

    跳轉路由

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/',function(){
-    return'welcome';
+Route::get('r2',function(){
+    return view('welcome');
 });
+
+Route::get('r1',function(){
+    return redirect('r2');
+});
+
+
 
 
 
