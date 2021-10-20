88d929ee8cd8fa36f7a1158eb748980132590766
SuYiXuan <3a832012@gm.student.ncut.edu.tw>
2021-10-20 11:32:45 +0800

    練習4 為route命名

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('hello/{name?}',function($name='Everybody'){
-return'Hello,'.$name;
-});
+
 
 Route::get('/',function(){
     return view('welcome');
 });
+
+Route::get('hello/{name?}',function($name='Everybody'){
+    return'Hello,'.$name;
+})->name('hello.index');
 
 
 
